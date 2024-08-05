d3c2a67e71ed3c12ad426972dc770e5d2f2bb6cf
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-08-05 19:48:23 +0700

    Test

--- make-build.sh
+++ make-build.sh
@@ -15,7 +15,13 @@ kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim
 libmbim libqmi modemmanager luci-proto-modemmanager usbutils luci-proto-mbim luci-proto-ncm luci-proto-qmi \
 kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless \
 kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-utils libimobiledevice-utils usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem \
-kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
+kmod-phy-broadcom kmod-phylib-broadcom"
+
+if [ "$BRANCH" == "snapshots" ]; then
+    # PACKAGES+=" "
+else
+    PACKAGES+=" kmod-tg3"
+fi
 
 # Modem Tools
 PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-lite-watchdog luci-app-3ginfo-lite picocom minicom"
