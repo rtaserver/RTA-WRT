415bba8dd858b3a1698c9af511e95ed3871870f1
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-09-18 06:26:05 +0000

    Update

--- make-build.sh
+++ make-build.sh
@@ -55,7 +55,7 @@ PACKAGES+=" luci-theme-material"
 PACKAGES+=" libc php8 php8-fastcgi php8-fpm coreutils-stat zoneinfo-asia php8-cgi \
 php8-cli php8-mod-bcmath php8-mod-calendar php8-mod-ctype php8-mod-curl php8-mod-dom php8-mod-exif \
 php8-mod-fileinfo php8-mod-filter php8-mod-gd php8-mod-iconv php8-mod-intl php8-mod-mbstring php8-mod-mysqli \
-php8-mod-mysqlnd php8-mod-opcache php8-mod-openssl php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar php8-mod-session \
+php8-mod-mysqlnd php8-mod-opcache php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar php8-mod-session \
 php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip"
 
 # Misc and some custom .ipk files
