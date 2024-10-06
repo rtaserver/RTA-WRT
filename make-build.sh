d57db88c4662a6155526fa2bccb0eee9eea26ec9
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-10-06 15:46:17 +0000

    P

--- make-build.sh
+++ make-build.sh
@@ -18,7 +18,7 @@ kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-uti
 kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
 PACKAGES+=" acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 \
 cgi-io chattr comgt comgt-ncm containerd coremark coreutils coreutils-base64 coreutils-nohup \
-coreutils-truncate curl docker docker-compose dockerd dosfstools dumpe2fs e2freefrag e2fsprogs \
+coreutils-truncate curl dosfstools dumpe2fs e2freefrag e2fsprogs \
 exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git gzip hostapd-common iconv iw iwinfo jq \
 jshn kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp \
 liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs openssl-util parted \
@@ -26,7 +26,7 @@ perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perl
 pigz ppp ppp-mod-pppoe proto-bonding pv rename resize2fs runc tar tini ttyd tune2fs \
 uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch uuidgen wget-ssl whereis \
 which wwan xfs-fsck xfs-mkfs xz xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
-luci luci-base luci-compat luci-lib-base luci-lib-docker \
+luci luci-base luci-compat luci-lib-base \
 luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
 luci-mod-status luci-mod-system luci-proto-bonding \
 luci-proto-ppp lolcat coreutils-stty"
