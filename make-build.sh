c5fbfbb1d0e15bede64d167ead0478b47e772676
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-10-09 00:52:00 +0700

    Update make-build.sh

--- make-build.sh
+++ make-build.sh
@@ -29,7 +29,7 @@ which wwan xfs-fsck xfs-mkfs xz xz-utils ziptool zoneinfo-asia zoneinfo-core zst
 luci luci-base luci-compat luci-lib-base \
 luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
 luci-mod-status luci-mod-system luci-proto-bonding \
-luci-proto-ppp lolcat coreutils-stty"
+luci-proto-ppp lolcat coreutils-stty git git-http"
 
 
 PACKAGES+=" luci-app-zerotier luci-app-cloudflared tailscale luci-app-tailscale"
