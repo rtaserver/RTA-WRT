6c6cc679e4d45ebe4a6fd0ccdbc18deda6e40cb3
xidz <fidkholiq@gmail.com>
2024-12-16 02:54:57 +0700

    Add files via upload

--- imagebuilder-24.sh
+++ imagebuilder-24.sh
@@ -524,14 +524,14 @@ rebuild_firmware() {
 
     # PHP8
     PACKAGES+=" php8 php8-cgi php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring coreutils-stat zoneinfo-asia bash curl tar"
-    #PACKAGES+=" libc php8 php8-fastcgi php8-fpm coreutils-stat zoneinfo-asia php8-cgi \
-    #php8-cli php8-mod-bcmath php8-mod-calendar php8-mod-ctype php8-mod-curl php8-mod-dom php8-mod-exif \
-    #php8-mod-fileinfo php8-mod-filter php8-mod-gd php8-mod-iconv php8-mod-intl php8-mod-mbstring php8-mod-mysqli \
-    #php8-mod-mysqlnd php8-mod-opcache php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar php8-mod-session \
-   # php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip libopenssl-legacy"
 
     # Misc and some custom .ipk files
-    misc+=" tailscale luci-app-tailscale luci-app-cpufreq luci-app-mmconfig luci-app-temp-status luci-app-rakitanmanager luci-theme-alpha luci-theme-argon luci-app-droidnet"
+    misc=""
+    if [ "$op_target" == "openwrt" ]; then
+        misc+=" tailscale luci-app-tailscale luci-app-cpufreq luci-app-mmconfig luci-app-temp-status luci-app-rakitanmanager luci-theme-alpha luci-theme-argon luci-app-droidnet"
+    elif [ "$op_target" == "immortalwrt" ]; then
+        misc+=" "
+    fi
     
     if [ "$op_target" == "rpi-4" ]; then
         misc+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio luci-app-oled"
