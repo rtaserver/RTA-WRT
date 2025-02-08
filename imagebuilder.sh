b867d9bb64f48d7901d77c3c42b9a3d472c55031
rizkikotet-dev <rizkidgc31@gmail.com>
2025-02-08 18:57:36 +0700

    Fix package list in imagebuilder.sh by removing redundant iperf3 entry and ensuring correct inclusion of iperf3-ssl

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -684,7 +684,7 @@ rebuild_firmware() {
     PACKAGES+=" luci-proto-ncm usb-modeswitch nano wget curl libusb-1.0-0 python3-pip"
     PACKAGES+=" kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 hostapd wpa-cli wpa-supplicant kmod-cfg80211 kmod-mac80211 wireless-tools iw-full hostapd-utils"
     PACKAGES+=" libqmi libqrtr-glib qmi-utils luci-proto-qmi mbim-utils libmbim luci-proto-mbim umbim php8 php8-cgi php8-fastcgi php8-fpm php8-mod-ctype php8-mod-curl php8-mod-fileinfo php8-mod-iconv php8-mod-mbstring php8-mod-session php8-mod-zip"
-    PACKAGES+=" perl perlbase-base perlbase-bytes perlbase-class perlbase-config perlbase-cwd perlbase-dynaloader perlbase-errno perlbase-essential perlbase-fcntl perlbase-file perlbase-filehandle perlbase-i18n perlbase-integer perlbase-io perlbase-list perlbase-locale perlbase-params perlbase-posix perlbase-re perlbase-scalar perlbase-selectsaver perlbase-socket perlbase-symbol perlbase-tie perlbase-time perlbase-unicore perlbase-utf8 perlbase-xsloader iperf3"
+    PACKAGES+=" perl perlbase-base perlbase-bytes perlbase-class perlbase-config perlbase-cwd perlbase-dynaloader perlbase-errno perlbase-essential perlbase-fcntl perlbase-file perlbase-filehandle perlbase-i18n perlbase-integer perlbase-io perlbase-list perlbase-locale perlbase-params perlbase-posix perlbase-re perlbase-scalar perlbase-selectsaver perlbase-socket perlbase-symbol perlbase-tie perlbase-time perlbase-unicore perlbase-utf8 perlbase-xsloader"
     PACKAGES+=" ruby ruby-bigdecimal ruby-date ruby-digest ruby-enc ruby-forwardable ruby-pstore ruby-psych ruby-stringio ruby-yaml"
     PACKAGES+=" luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-lua-runtime zoneinfo-asia zoneinfo-core"
     PACKAGES+=" kmod-usb-serial kmod-usb-serial-option unzip tar gzip openssh-sftp-server sms-tool luci-app-temp-status cpusage adb ttyd luci-app-ttyd bash dmesg screen kmod-tun jq luci-lib-ipkg"
@@ -713,7 +713,7 @@ rebuild_firmware() {
     PACKAGES+=" internet-detector luci-app-internet-detector internet-detector-mod-modem-restart nlbwmon luci-app-nlbwmon vnstat2 vnstati2 luci-app-vnstat2 netdata"
 
     # Speedtest
-    PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 -iperf3-ssl homebox luci-app-netspeedtest"
+    PACKAGES+=" librespeed-go python3-speedtest-cli iperf3-ssl homebox luci-app-netspeedtest"
 
     # Theme
     PACKAGES+=" luci-theme-material luci-theme-argon luci-app-argon-config luci-theme-material3"
