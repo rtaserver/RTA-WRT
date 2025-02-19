7a8c1b9a738a8938072bfb5844616aef22d5f8b4
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2025-02-19 20:52:02 +0700

    Update MISC.sh

--- scripts/MISC.sh
+++ scripts/MISC.sh
@@ -2,58 +2,123 @@
 
 . ./scripts/INCLUDE.sh
 
-echo "Start Downloading Misc files and setup configuration!"
-echo "Current Path: $PWD"
+# Logging functions
+log() {
+    echo "[INFO] $1"
+}
 
-#setup custom setting for openwrt and immortalwrt
-sed -i "s/Ouc3kNF6/$DATE/g" files/etc/uci-defaults/99-init-settings.sh
-if [[ "$BASE" == "openwrt" ]]; then
-    echo "$BASE"
-    sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
-elif [[ "$BASE" == "immortalwrt" ]]; then
-    echo "$BASE"
-fi
+error() {
+    echo "[ERROR] $1"
+    exit 1
+}
 
-if [ "$TYPE" == "AMLOGIC" ]; then
-    rm files/etc/uci-defaults/70-rootpt-resize
-    rm files/etc/uci-defaults/80-rootfs-resize
-    rm files/etc/sysupgrade.conf
-fi
+# Initialize environment
+init_environment() {
+    log "Start Downloading Misc files and setup configuration!"
+    log "Current Path: $PWD"
+}
 
-# add yout custom command for specific target and release branch version here
-if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "24" ]; then
-    echo "$BRANCH"
-elif [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "23" ]; then
-    echo "$BRANCH"
-fi
+# Setup base-specific configurations
+setup_base_config() {
+    # Update date in init settings
+    sed -i "s/Ouc3kNF6/$DATE/g" files/etc/uci-defaults/99-init-settings.sh
+    
+    case "$BASE" in
+        "openwrt")
+            log "Configuring OpenWrt specific settings"
+            sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
+            ;;
+        "immortalwrt")
+            log "Configuring ImmortalWrt specific settings"
+            ;;
+        *)
+            log "Unknown base system: $BASE"
+            ;;
+    esac
+}
 
-if [ "$TYPE" == "AMLOGIC" ]; then
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/3g.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/dhcp.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/dhcpv6.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/ncm.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/wwan.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/wireless/mac80211.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp-get-server.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp.script' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcpv6.script' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/hostapd.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/netifd-proto.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/netifd-wireless.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/utils.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/wifi/mac80211.sh' files/etc/uci-defaults/99-init-settings.sh
-else
-    rm -rf files/lib
-fi
+# Handle Amlogic-specific files
+handle_amlogic_files() {
+    if [ "$TYPE" == "AMLOGIC" ]; then
+        log "Removing Amlogic-specific files"
+        rm -f files/etc/uci-defaults/70-rootpt-resize
+        rm -f files/etc/uci-defaults/80-rootfs-resize
+        rm -f files/etc/sysupgrade.conf
+    fi
+}
 
-# custom script files urls
-echo "Downloading custom script" 
-sync_time="https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh"
-clock="https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock"
-repair_ro="https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/install2.sh"
+# Setup branch-specific configurations
+setup_branch_config() {
+    local branch_major=$(echo "$BRANCH" | cut -d'.' -f1)
+    case "$branch_major" in
+        "24")
+            log "Configuring for branch 24.x"
+            ;;
+        "23")
+            log "Configuring for branch 23.x"
+            ;;
+        *)
+            log "Unknown branch version: $BRANCH"
+            ;;
+    esac
+}
 
-wget --no-check-certificate -nv -P files/sbin "$sync_time"
-wget --no-check-certificate -nv -P files/usr/bin "$clock"
-wget --no-check-certificate -nv -P files/root "$repair_ro"
+# Configure file permissions for Amlogic
+configure_amlogic_permissions() {
+    if [ "$TYPE" == "AMLOGIC" ]; then
+        log "Setting up Amlogic file permissions"
+        local netifd_files=(
+            "/lib/netifd/proto/3g.sh"
+            "/lib/netifd/proto/dhcp.sh"
+            "/lib/netifd/proto/dhcpv6.sh"
+            "/lib/netifd/proto/ncm.sh"
+            "/lib/netifd/proto/wwan.sh"
+            "/lib/netifd/wireless/mac80211.sh"
+            "/lib/netifd/dhcp-get-server.sh"
+            "/lib/netifd/dhcp.script"
+            "/lib/netifd/dhcpv6.script"
+            "/lib/netifd/hostapd.sh"
+            "/lib/netifd/netifd-proto.sh"
+            "/lib/netifd/netifd-wireless.sh"
+            "/lib/netifd/utils.sh"
+            "/lib/wifi/mac80211.sh"
+        )
+        
+        for file in "${netifd_files[@]}"; do
+            sed -i "/# setup misc settings/ a\chmod +x $file" files/etc/uci-defaults/99-init-settings.sh
+        done
+    else
+        log "Removing lib directory for non-Amlogic build"
+        rm -rf files/lib
+    fi
+}
 
-echo "All custom configuration setup completed!"
+# Download custom scripts
+download_custom_scripts() {
+    log "Downloading custom scripts"
+    
+    local scripts=(
+        "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh|files/sbin"
+        "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/usr/bin/clock|files/usr/bin"
+        "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/install2.sh|files/root"
+    )
+    
+    for script in "${scripts[@]}"; do
+        IFS='|' read -r url path <<< "$script"
+        wget --no-check-certificate -nv -P "$path" "$url" || error "Failed to download: $url"
+    done
+}
+
+# Main execution
+main() {
+    init_environment
+    setup_base_config
+    handle_amlogic_files
+    setup_branch_config
+    configure_amlogic_permissions
+    download_custom_scripts
+    log "All custom configuration setup completed!"
+}
+
+# Execute main function
+main
