c0df54aa3a369f5646e8a3a8692f2cab9af84b56
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2025-02-20 20:15:48 +0700

    Update MISC.sh

--- scripts/MISC.sh
+++ scripts/MISC.sh
@@ -2,20 +2,10 @@
 
 . ./scripts/INCLUDE.sh
 
-# Logging functions
-log() {
-    echo "[INFO] $1"
-}
-
-error() {
-    echo "[ERROR] $1"
-    exit 1
-}
-
 # Initialize environment
 init_environment() {
-    log "Start Downloading Misc files and setup configuration!"
-    log "Current Path: $PWD"
+    log "INFO" "Start Downloading Misc files and setup configuration!"
+    log "INFO" "Current Path: $PWD"
 }
 
 # Setup base-specific configurations
@@ -25,14 +15,14 @@ setup_base_config() {
     
     case "$BASE" in
         "openwrt")
-            log "Configuring OpenWrt specific settings"
+            log "INFO" "Configuring OpenWrt specific settings"
             sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
             ;;
         "immortalwrt")
-            log "Configuring ImmortalWrt specific settings"
+            log "INFO" "Configuring ImmortalWrt specific settings"
             ;;
         *)
-            log "Unknown base system: $BASE"
+            log "INFO" "Unknown base system: $BASE"
             ;;
     esac
 }
@@ -40,7 +30,7 @@ setup_base_config() {
 # Handle Amlogic-specific files
 handle_amlogic_files() {
     if [ "$TYPE" == "AMLOGIC" ]; then
-        log "Removing Amlogic-specific files"
+        log "INFO" "Removing Amlogic-specific files"
         rm -f files/etc/uci-defaults/70-rootpt-resize
         rm -f files/etc/uci-defaults/80-rootfs-resize
         rm -f files/etc/sysupgrade.conf
@@ -52,13 +42,13 @@ setup_branch_config() {
     local branch_major=$(echo "$BRANCH" | cut -d'.' -f1)
     case "$branch_major" in
         "24")
-            log "Configuring for branch 24.x"
+            log "INFO" "Configuring for branch 24.x"
             ;;
         "23")
-            log "Configuring for branch 23.x"
+            log "INFO" "Configuring for branch 23.x"
             ;;
         *)
-            log "Unknown branch version: $BRANCH"
+            log "INFO" "Unknown branch version: $BRANCH"
             ;;
     esac
 }
@@ -66,7 +56,7 @@ setup_branch_config() {
 # Configure file permissions for Amlogic
 configure_amlogic_permissions() {
     if [ "$TYPE" == "AMLOGIC" ]; then
-        log "Setting up Amlogic file permissions"
+        log "INFO" "Setting up Amlogic file permissions"
         local netifd_files=(
             "/lib/netifd/proto/3g.sh"
             "/lib/netifd/proto/dhcp.sh"
@@ -88,14 +78,14 @@ configure_amlogic_permissions() {
             sed -i "/# setup misc settings/ a\chmod +x $file" files/etc/uci-defaults/99-init-settings.sh
         done
     else
-        log "Removing lib directory for non-Amlogic build"
+        log "INFO" "Removing lib directory for non-Amlogic build"
         rm -rf files/lib
     fi
 }
 
 # Download custom scripts
 download_custom_scripts() {
-    log "Downloading custom scripts"
+    log "INFO" "Downloading custom scripts"
     
     local scripts=(
         "https://raw.githubusercontent.com/frizkyiman/auto-sync-time/main/sbin/sync_time.sh|files/sbin"
@@ -117,7 +107,7 @@ main() {
     setup_branch_config
     configure_amlogic_permissions
     download_custom_scripts
-    log "All custom configuration setup completed!"
+    log "SUCCESS" "All custom configuration setup completed!"
 }
 
 # Execute main function
